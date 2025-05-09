316fa0e7eca9c60da39abebdaf71dd18f5952a2e
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-09 16:51:07 +0500

    Initial Commit

---
+++ EduMath/EduMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduMathApp.swift
+//  EduMath
+//
+//  Created by Fazliddin Abdazimov on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct EduMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EduMathTests/EduMathTests.swift
@@ -0,0 +1,17 @@
+//
+//  EduMathTests.swift
+//  EduMathTests
+//
+//  Created by Fazliddin Abdazimov on 09/05/25.
+//
+
+import Testing
+@testable import EduMath
+
+struct EduMathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
