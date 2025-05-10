b301c929470abbb7db94f5f8907f63c9a8948762
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-10 17:01:17 +0500

    project started

--- EduMath/ContentView.swift
+++ EduMath/ContentView.swift
@@ -2,20 +2,31 @@
 //  ContentView.swift
 //  EduMath
 //
-//  Created by Fazliddin Abdazimov on 09/05/25.
+//  Created by HiRO on 09/05/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    private var multiplicationTableNumbers: [Int] = Array(2...20)
+    
+    @State private var randomSelectedFirstNumber = Int.random(in: 2...20)
+    @State private var randomSelectedSecondNumber = Int.random(in: 1...12)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("\(randomSelectedFirstNumber) x \(randomSelectedSecondNumber) = ?")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            TextField("Your answer", text: .constant(""))
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .frame(width: 200, height: 50, alignment: .center)
+                .cornerRadius(8)
+                .keyboardType(.numberPad)
         }
-        .padding()
     }
 }
 
