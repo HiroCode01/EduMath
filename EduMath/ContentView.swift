927338962e8f27634957cd6371fe6a2ec0a98673
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-13 03:37:07 +0500

    added function and view modifier to button

--- EduMath/ContentView.swift
+++ EduMath/ContentView.swift
@@ -8,28 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var multiplicationTableNumbers: [Int] = Array(2...20)
+    @State private var randomSelectedFirstNumber:  Int
+    @State private var randomSelectedSecondNumber: Int
+    @State private var answerOptions: [Int]
     
-    @State private var randomSelectedFirstNumber = Int.random(in: 2...20)
-    @State private var randomSelectedSecondNumber = Int.random(in: 1...12)
+    init() {
+        let first  = Int.random(in: 2...20)
+        let second = Int.random(in: 1...12)
+        
+        _randomSelectedFirstNumber  = State(initialValue: first)
+        _randomSelectedSecondNumber = State(initialValue: second)
+        _answerOptions              = State(initialValue: buttonAnswerAndRandomAnswers(first, second))
+    }
+    
+    @State private var scorePoints: Int = 0
+    @State private var isGameOver: Bool = false
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var body: some View {
         VStack {
-            
             Text("\(randomSelectedFirstNumber) x \(randomSelectedSecondNumber) = ?")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
-            TextField("Your answer", text: .constant(""))
-                .padding()
-                .background(Color.gray.opacity(0.2))
-                .frame(width: 200, height: 50, alignment: .center)
-                .cornerRadius(8)
-                .keyboardType(.numberPad)
+            Spacer().frame(height: 200)
+            
+            LazyVGrid(columns: columns, alignment: .center, spacing: 20){
+                ForEach(answerOptions, id: \.self) { option in
+                    Button("\(option)") {
+                        //Something
+                    }
+                    .mainButtonStyle()
+                }
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+//View Design For Main Buttons
+struct MainButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title2)
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.white)
+            .background(Color.blue)
+            .clipShape(.rect(cornerRadius: 15))
+    }
+}
+
+extension View {
+    func mainButtonStyle() -> some View {
+        modifier(MainButtonStyle())
+    }
+}
+
+//Function to get an asnwer and 3 random wrong asnwers for buttons
+func buttonAnswerAndRandomAnswers(_ firstNumber: Int, _ secondNumber: Int) -> Array<Int> {
+    let correct = firstNumber * secondNumber
+    var answers = [correct]
+    
+    while answers.count < 4 {
+        let wrong = Int.random(in: 1...12 * firstNumber)
+        
+        if !answers.contains(wrong) {
+            answers.append(wrong)
+        }
+    }
+    return answers.shuffled()
 }
